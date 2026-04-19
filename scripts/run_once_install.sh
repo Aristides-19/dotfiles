4382a672a36f0d24f37d03c8515d9e3351070c13
Arístides Pérez <aristp.2005@gmail.com>
2026-04-19 17:00:37 -0400

    feat: add run once install sh script

---
+++ scripts/run_once_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# =============================================================================
+# INSTALL PACKAGES SCRIPT
+# This script runs once to set up the
+# environment on CachyOS/Arch Linux.
+# =============================================================================
+
+set -e # Exit on error
+
+echo "Starting install process..."
+
+echo "update: Synchronizing repos..."
+sudo pacman -Syu --noconfirm
+
+PACKAGES=(
+    "kitty"
+    "zsh"
+    "zsh-theme-powerlevel10k"
+    "zsh-autosuggestions"
+    "zsh-syntax-highlighting"
+    "fastfetch"
+    "code"
+)
+
+echo "Installing packages..."
+paru -S --needed --noconfirm "${PACKAGES[@]}"
+
+if [ "$SHELL" != "/usr/bin/zsh" ]; then
+    echo "Changing default shell to Zsh..."
+    chsh -s /usr/bin/zsh
+fi
+
+echo "Package installation complete!"
