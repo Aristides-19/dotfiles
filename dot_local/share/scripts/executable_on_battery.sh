aa1c07a725d6d8da30aedd1104021b3ae774c66d
Arístides Pérez <aristp.2005@gmail.com>
2026-09-19 04:53:38 -0400

    feat: add ASUS configuration scripts and refresh rate management

--- dot_local/share/scripts/executable_on_battery.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-## 60Hz
-MONITOR=$(hyprctl monitors -j | jq -r '.[] | select(.name | test("eDP")).name')
-hyprctl eval "hl.monitor({ output = \"$MONITOR\", mode = \"1920x1200@60\", position = \"auto\", scale = \"1\" })"
